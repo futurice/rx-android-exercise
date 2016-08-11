cd2b1e34e14d794fe77452883f39be93c3dc2a99
guillaumevalverde <guillaume.valverde@gmail.com>
2016-08-11 16:10:13 +0100

    add exercise using combinelatest, flatmap switchmap...

---
+++ src/main/java/com/futurice/rxandroidtest/articleexercise/ExerciseArticle.java
@@ -0,0 +1,68 @@
+package com.futurice.rxandroidtest.articleexercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.Observable;
+
+/**
+ * Created by gval on 11/08/2016.
+ */
+public class ExerciseArticle {
+    private final DataLayer dataLayer;
+
+    public ExerciseArticle(DataLayer dataLayer) {
+        this.dataLayer = dataLayer;
+    }
+
+    /**
+     * create a function which merge both ids stream to create one list,
+     * for each id , use the datalayer.fetcharticle() to retrieve the article.
+     *
+     * only article with a even id interest us.
+     *
+     *
+     * @param articleIdsObservable1 gives a stream of list<ids>,  this list can refresh and should refresh the total list
+     * @param articleIdsObservable2 gives a stream of list<ids> from another source
+     * @return Observable<List<Article>> merge the two list and for each id fetch the corresponding article.
+     */
+    public Observable<List<Article>> getArticleStream(Observable<List<Integer>> articleIdsObservable1,
+                                                      Observable<List<Integer>> articleIdsObservable2) {
+
+        return Observable.combineLatest(articleIdsObservable1, articleIdsObservable2,
+                (listOne, listTwo) -> {
+                    List<Integer> newList = new ArrayList<>(listOne);
+                    newList.addAll(listTwo);
+                    return newList;
+                })
+                .flatMap(list -> Observable.from(list)
+                        .filter(id -> id % 2 == 0)
+                        .doOnNext(id -> System.out.println(id))
+                        .flatMap(dataLayer::fetchArticle)
+                        .toList());
+    }
+
+    /**
+     * same as above but optimise it that if a new list comes up from one of the two observables
+     * articleIdsObservable1 or articleIdsObservable2, it cancel all fetching and focus on new list
+     *
+     * @param articleIdsObservable1 gives a stream of list<ids>,  this list can refresh and should refresh the total list
+     * @param articleIdsObservable2 gives a stream of list<ids> from another source
+     * @return
+     */
+    public Observable<List<Article>> getArticleStreamOptimised(Observable<List<Integer>> articleIdsObservable1,
+                                                      Observable<List<Integer>> articleIdsObservable2) {
+
+        return Observable.combineLatest(articleIdsObservable1, articleIdsObservable2,
+                (listOne, listTwo) -> {
+                    List<Integer> newList = new ArrayList<>(listOne);
+                    newList.addAll(listTwo);
+                    return newList;
+                })
+                .switchMap(list -> Observable.from(list)
+                        .filter(id -> id % 2 == 0)
+                        .doOnNext(id -> System.out.println(id))
+                        .flatMap(dataLayer::fetchArticle)
+                        .toList());
+    }
+}
